afac1a9aac105873219fc5ea9093d3c2526335a9
SergeiT <uraniumcore238@gmail.com>
2024-04-17 22:38:25 +0300

    added fast api

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,33 +1,12 @@
 import datetime
-import os
 import pytest
+
 from faker import Faker
-
-from receipts.text_to_sum import extract_total_lines
-
-
-# @pytest.fixture()
-# def file_path() -> str:
-#     path_to_file = 'test.txt'
-#     with open(path_to_file, 'w') as file_handler:
-#         file_handler.writelines(['0,00\n2,27\n2,27\nTotal\n1,63\n34,77\n36,40\n0124 692544/03\n15.02.24 18:55'])
-#     yield path_to_file
-#     os.remove(path_to_file)
-
-# from unittest.mock import mock_open, patch
-
-
-# @pytest.fixture
-# def file_path():
-#     m = mock_open()
-#     with patch('__main__.open', m):
-#         with open('test.txt', 'w') as file_handler:
-#             b = file_handler.writelines(['0,00\n2,27\n2,27\nTotal\n1,63\n34,77\n36,40\n0124 692544/03\n15.02.24 18:55'])
-#         return file_handler
 
 
 NOT_SET = '___'
-# faker = Faker
+
+
 @pytest.fixture
 def make_receipt(faker):
     faker = Faker
@@ -40,6 +19,3 @@ def make_receipt(faker):
         return '\n'.join([title_part, items_part, total_part, end_part])
 
     return inner
-
-
-
